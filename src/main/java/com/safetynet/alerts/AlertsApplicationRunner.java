0f6d243ce71e8ddb62972befa228ea44711de99f
betty Domain <betty.domain@free.fr>
2020-12-17 15:07:15 +0100

    Ajout de propriété pour le lancement de ce traitement afin de ne pas l'effectuer systématiquement lors des tests unitaires par exemple

--- src/main/java/com/safetynet/alerts/AlertsApplicationRunner.java
+++ src/main/java/com/safetynet/alerts/AlertsApplicationRunner.java
@@ -4,8 +4,14 @@ package com.safetynet.alerts;
 import com.safetynet.alerts.controller.AlertsApplicationRunnerController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+@ConditionalOnProperty(
+        prefix = "application.runner",
+        value = "enabled",
+        havingValue = "true",
+        matchIfMissing = true)
 @Component
 public class AlertsApplicationRunner implements CommandLineRunner {
 
